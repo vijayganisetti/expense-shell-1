2a94ea68350a075465b48d0c5700c974c2d2125b
vijayganisetti <ganisettisai@gmail.com>
2024-07-01 17:47:35 +0530

    first

--- frontend.sh
+++ frontend.sh
@@ -5,30 +5,30 @@ source ./common.sh
 common_script 
 
 dnf install nginx -y &>>$LOGFILE
-VALIDATE $? "Installing nginx"
+#VALIDATE $? "Installing nginx"
 
 systemctl enable nginx &>>$LOGFILE
-VALIDATE $? "Enabling nginx"
+#VALIDATE $? "Enabling nginx"
 
 systemctl start nginx &>>$LOGFILE
-VALIDATE $? "Starting nginx"
+#VALIDATE $? "Starting nginx"
 
 #rm -rf /usr/share/nginx/html/* &>>$LOGFILE
 rm -rf /usr/share/nginx/html/* &>>$LOGFILE
-VALIDATE $? "Removing existing content"
+#VALIDATE $? "Removing existing content"
 
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
-VALIDATE $? "Downloading frontend code"
+#VALIDATE $? "Downloading frontend code"
 
 cd /usr/share/nginx/html &>>$LOGFILE
 unzip /tmp/frontend.zip &>>$LOGFILE
-VALIDATE $? "Extracting frontend code"
+#VALIDATE $? "Extracting frontend code"
 
 #check your repo and path
 ## ## whiel doing manually in linux we use vim editor but in vscode we need to seperate file with .conf (expense.conf)
 
 cp /home/ec2-user/expense-shell-1/expense.conf  /etc/nginx/default.d/expense.conf &>>$LOGFILE
-VALIDATE $? "Copied expense conf"
+#VALIDATE $? "Copied expense conf"
 
 systemctl restart nginx &>>$LOGFILE
-VALIDATE $? "Restarting nginx"
+#VALIDATE $? "Restarting nginx"
