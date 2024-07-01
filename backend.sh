f07018e8849466156d6626c3727dea226c10a9d3
vijayganisetti <ganisettisai@gmail.com>
2024-07-01 17:44:21 +0530

    first

--- backend.sh
+++ backend.sh
@@ -4,62 +4,62 @@ source ./common.sh
 common_script 
 
 dnf module disable nodejs -y &>>$LOGFILE
-VALIDATE $? "Disabling default nodejs"
+#VALIDATE $? "Disabling default nodejs"
 
 dnf module enable nodejs:20 -y &>>$LOGFILE
-VALIDATE $? "Enabling nodejs:20 version"
+#VALIDATE $? "Enabling nodejs:20 version"
 
 dnf install nodejs -y &>>$LOGFILE
-VALIDATE $? "Installing nodejs"
+#VALIDATE $? "Installing nodejs"
 
 ## we cannot add user multiple times with same name so we need to take care of it. this is not idemponent
 id expense &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     useradd expense &>>$LOGFILE
-    VALIDATE $? "Creating expense user"
+    #VALIDATE $? "Creating expense user"
 else
     echo -e "Expense user already created...$Y SKIPPING $N"
 fi
 
 ## we cannot create directory multiple times with same name . simply we can keep -p after mkdir so we need to take care of it.
 mkdir -p /app &>>$LOGFILE
-VALIDATE $? "Creating app directory"
+#VALIDATE $? "Creating app directory"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
-VALIDATE $? "Downloading backend code"
+#VALIDATE $? "Downloading backend code"
 
 cd /app
 rm -rf /app/* ## we cannot unzip multiple times .so here rm -rf/app/* removes the content inside the app directory
 unzip /tmp/backend.zip &>>$LOGFILE
-VALIDATE $? "Extracted backend code"
+#VALIDATE $? "Extracted backend code"
 
 npm install &>>$LOGFILE
-VALIDATE $? "Installing nodejs dependencies"
+#VALIDATE $? "Installing nodejs dependencies"
 
 
 ## whiel doing manually in linux we use vim editor but in vscode we need to seperate file with .service extension (backend.service)
 #check your repo and path / and use cp path
 cp /home/ec2-user/expense-shell-1/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
-VALIDATE $? "Copied backend service"
+#VALIDATE $? "Copied backend service"
 
 systemctl daemon-reload &>>$LOGFILE
-VALIDATE $? "Daemon Reload"
+#VALIDATE $? "Daemon Reload"
 
 systemctl start backend &>>$LOGFILE
-VALIDATE $? "Starting backend"
+#VALIDATE $? "Starting backend"
 
 systemctl enable backend &>>$LOGFILE
-VALIDATE $? "Enabling backend"
+#VALIDATE $? "Enabling backend"
 
 dnf install mysql -y &>>$LOGFILE
-VALIDATE $? "Installing MySQL Client"
+#VALIDATE $? "Installing MySQL Client"
 
 mysql -h db.vijayganisetti.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
-VALIDATE $? "Schema loading"
+#VALIDATE $? "Schema loading"
 
 systemctl restart backend &>>$LOGFILE
-VALIDATE $? "Restarting Backend"
+#VALIDATE $? "Restarting Backend"
 
 
 
