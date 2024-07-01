db39bbe92836a760a7d2986ebbe810fa0453c4a6
vijayganisetti <ganisettisai@gmail.com>
2024-07-01 14:35:52 +0530

    first

---
+++ backend.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+echo "Please enter DB password:"
+read -s mysql_root_password
+
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+dnf module disable nodejs -y &>>$LOGFILE
+VALIDATE $? "Disabling default nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOGFILE
+VALIDATE $? "Enabling nodejs:20 version"
+
+dnf install nodejs -y &>>$LOGFILE
+VALIDATE $? "Installing nodejs"
+
+## we cannot add user multiple times with same name so we need to take care of it. this is not idemponent
+id expense &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    useradd expense &>>$LOGFILE
+    VALIDATE $? "Creating expense user"
+else
+    echo -e "Expense user already created...$Y SKIPPING $N"
+fi
+
+## we cannot create directory multiple times with same name . simply we can keep -p after mkdir so we need to take care of it.
+mkdir -p /app &>>$LOGFILE
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
+VALIDATE $? "Downloading backend code"
+
+cd /app
+rm -rf /app/* ## we cannot unzip multiple times .so here rm -rf/app/* removes the content inside the app directory
+unzip /tmp/backend.zip &>>$LOGFILE
+VALIDATE $? "Extracted backend code"
+
+npm install &>>$LOGFILE
+VALIDATE $? "Installing nodejs dependencies"
+
+
+## whiel doing manually in linux we use vim editor but in vscode we need to seperate file with .service extension (backend.service)
+#check your repo and path / and use cp path
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+VALIDATE $? "Copied backend service"
+
+systemctl daemon-reload &>>$LOGFILE
+VALIDATE $? "Daemon Reload"
+
+systemctl start backend &>>$LOGFILE
+VALIDATE $? "Starting backend"
+
+systemctl enable backend &>>$LOGFILE
+VALIDATE $? "Enabling backend"
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL Client"
+
+mysql -h db.vijayganisetti.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+VALIDATE $? "Schema loading"
+
+systemctl restart backend &>>$LOGFILE
+VALIDATE $? "Restarting Backend"
+
+
+
---
+++ mysql.sh
@@ -0,0 +1,76 @@
+# #!/bin/bash
+
+# USERID=$(id -u)
+# TIMESTAMP=$(date +%F-%H-%M-%S)
+# SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+# LOGFILE=/tmp/$TIMESTAMP-$SCRIPTNAME.log
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
+
+
+# VALIDATE() {
+#     if [ $1 -ne 0 ]
+# then
+# echo -e "$2..$R is failure $N"
+# exit 1
+# else
+# echo -e "$2...$G is success $N"
+# fi 
+# }
+
+# if [ $USERID -ne 0 ]
+# then
+# echo "plz run with sudo user"
+# exit 1
+# else 
+# echo "you are a super user"
+# fi
+
+# dnf install mysql-server -y &>>$LOGFILE
+# VALIDATE $? "installing mysql"
+
+# systemctl enable mysqld &>>$LOGFILE
+# VALIDATE $? "enabling mysqld"   
+
+# systemctl start mysqld &>>$LOGFILE
+# VALIDATE $? "staring the mysqld"
+
+# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+# VALIDATE $? "setting up the root password"
+
+#!/bin/bash
+
+source ./common.sh
+
+common_script
+
+
+dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL Server"
+
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "Enabling MySQL Server"
+
+systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "Starting MySQL Server"
+
+#   mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+#   VALIDATE $? "Setting up root password"
+
+##1. idempotent is a nature of every programming language irrespective of how many times you execute the code it will show same result 
+##2. sshell is not idemponent nature we need to take care of it some commands will fail when we run them multiple times
+
+### $ mysql -h db.vijayganisetti.online<idaddress ofmysql server) -uroot -pExpenseApp@1 -e 'SHOW DATABASES;' --> this command is to check mysql is connecting to db server or not.
+
+
+### Below code will be useful for idempotent nature
+mysql -h db.vijayganisetti.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+    VALIDATE $? "MySQL Root password Setup"
+else
+    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+fi
